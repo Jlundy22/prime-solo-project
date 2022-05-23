4df7188ac4412a4fbf446d1b85477c9751a280ab
Jack Lund <jacklund920@gmail.com>
2022-05-23 13:00:51 -0500

    starting browse page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import BrowsePage from '../BrowsePage/BrowsePage';
 
 import './App.css';
 
@@ -66,6 +67,14 @@ function App() {
             path="/info"
           >
             <InfoPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            // logged in shows InfoPage else shows LoginPage
+            exact
+            path="/browse"
+          >
+            <BrowsePage />
           </ProtectedRoute>
 
           <Route
